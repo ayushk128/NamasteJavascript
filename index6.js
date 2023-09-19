9d9f425109ad51eb54e5f8e8ae87a98783ef208c
Kumar <ayushkumar6@deloitte.com>
2023-09-19 12:19:17 +0530

    Added Shadowing concepts

--- index6.js
+++ index6.js
@@ -1,9 +1,22 @@
-var a = 100
+// Block scope and shadowing
+// var a = 100
+// {
+//     var a = 10;
+//     let b = 15;
+//     const c = 20;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// console.log(a);
+
+let b = 100
 {
     var a = 10;
     let b = 15;
     const c = 20;
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
-console.log(a);
-// console.log(b);
-// console.log(c);
+console.log(b);
